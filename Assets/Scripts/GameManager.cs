6b83936e501dfbe7c7f5ed09ef058ea515f7b5a2
Alfred-Kang <alfredkangjr@gmail.com>
2024-07-26 16:06:16 +0800

    feat: imported assets and created main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,177 @@
+/*
+ * Author: Alfred Kang Jing Rui
+ * Date Created: 24/06/2024
+ * Date Modified: 25/06/2024
+ * Description: GameManager Script
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using StarterAssets;
+
+/// <summary>
+/// Manages the game state, player health, scene transitions, and UI updates.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    /// <summary>
+    /// The player's current health.
+    /// </summary>
+    public int playerHealth = 75;
+
+    /// <summary>
+    /// The player's maximum health.
+    /// </summary>
+    private int playerMaxHealth = 75;
+
+    /// <summary>
+    /// The number of objective collectibles obtained by the player.
+    /// </summary>
+    public int partsObtained = 0;
+
+    /// <summary>
+    /// Indicates if a new scene has been loaded.
+    /// </summary>
+    public bool newScene = false;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Slider playerHealthBar;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private StarterAssetsInputs _inputs;
+    [SerializeField] private GameObject deathMenu;
+    [SerializeField] private GameObject winMenu;
+    [SerializeField] private GameObject HUD;
+    [SerializeField] private GameObject bgBlur;
+
+    /// <summary>
+    /// The starting positions for each scene by index.
+    /// </summary>
+    public Vector3[] sceneIndexStartPos = new Vector3[]
+    {
+        Vector3.zero,                                           // main menu
+        new Vector3(-25.1399994f, 0, -78.4000015f),             // spaceship
+        new Vector3(-23.0100002f, -0.0799999982f, -114.620003f),// world
+        new Vector3(8.89999962f, 43.9000015f, -200.199997f)     // cave
+    };
+
+    private void FixedUpdate()
+    {
+        if (newScene == true)
+        {
+            UpdateHealthUI();
+            player.transform.localPosition = sceneIndexStartPos[SceneManager.GetActiveScene().buildIndex];
+            player.transform.rotation = Quaternion.Euler(Vector3.zero);
+            newScene = false;
+        }
+
+        if (_inputs.interact)
+        {
+            _inputs.interact = false;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        UpdateHealthUI();
+    }
+
+    /// <summary>
+    /// Moves the player to the next scene in the build index.
+    /// </summary>
+    public void MoveUpScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Moves the player to a specific scene by index.
+    /// </summary>
+    /// <param name="sceneIndex">The index of the scene to move to.</param>
+    public void MoveToScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        newScene = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Applies damage to the player.
+    /// </summary>
+    /// <param name="damage">The amount of damage to apply.</param>
+    public void PlayerDamage(int damage)
+    {
+        playerHealth -= damage;
+        UpdateHealthUI();
+
+        if (playerHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    /// <summary>
+    /// Handles the player's death, triggering the death menu and pausing the game.
+    /// </summary>
+    private void Death()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        deathMenu.SetActive(true);
+        HUD.SetActive(false);
+        bgBlur.SetActive(true);
+    }
+
+    /// <summary>
+    /// Handles the player winning, triggering the win menu and pausing the game.
+    /// </summary>
+    public void Win()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        winMenu.SetActive(true);
+        HUD.SetActive(false);
+        bgBlur.SetActive(true);
+    }
+
+    /// <summary>
+    /// Heals the player by a specified value.
+    /// </summary>
+    /// <param name="healValue">The amount of health to restore.</param>
+    public void PlayerHeal(int healValue)
+    {
+        playerHealth += healValue;
+        if (playerHealth > playerMaxHealth)
+        {
+            playerHealth = playerMaxHealth;
+        }
+        UpdateHealthUI();
+    }
+
+    /// <summary>
+    /// Updates the player's health UI elements.
+    /// </summary>
+    public void UpdateHealthUI()
+    {
+        healthText.SetText(playerHealth.ToString() + "/" + playerMaxHealth.ToString());
+        playerHealthBar.value = playerHealth;
+        playerHealthBar.maxValue = playerMaxHealth;
+    }   
+}
--- Assets/Starter Assets/Runtime/InputSystem/StarterAssetsInputs.cs
+++ Assets/Starter Assets/Runtime/InputSystem/StarterAssetsInputs.cs
@@ -5,78 +5,112 @@ using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : MonoBehaviour
-	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
-		public bool aim;
-		public bool shoot;
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+        public bool shoot;
+        public bool reload;
+        public bool slide;
+        public bool primary;
+        public bool secondary;
+        public bool pause;
+        public bool crouch;
+        public bool interact;
+        public bool aim;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
+        public void OnMove(InputValue value)
+        {
+            MoveInput(value.Get<Vector2>());
+        }
 
-		public void OnLook(InputValue value)
-		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
-		}
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
 
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
 
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
-
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
         public void OnAim(InputValue value)
         {
             AimInput(value.isPressed);
         }
-
         public void OnShoot(InputValue value)
         {
             ShootInput(value.isPressed);
         }
+        public void OnReload(InputValue value)
+        {
+            ReloadInput(value.isPressed);
+        }
+        public void OnSlide(InputValue value)
+        {
+            SlideInput(value.isPressed);
+        }
+        public void OnPrimary(InputValue value)
+        {
+            PrimaryInput(value.isPressed);
+        }
+        public void OnSecondary(InputValue value)
+        {
+            SecondaryInput(value.isPressed);
+        }
+        public void OnPause(InputValue value)
+        {
+            PauseInput(value.isPressed);
+        }
+        public void OnCrouch(InputValue value)
+        {
+            CrouchInput(value.isPressed);
+        }
+        public void OnInteract(InputValue value)
+        {
+            InteractInput(value.isPressed);
+        }
+
 #endif
 
 
         public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
+        {
+            move = newMoveDirection;
+        }
 
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
 
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
 
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
 
         public void AimInput(bool newAimState)
         {
@@ -88,15 +122,50 @@ namespace StarterAssets
             shoot = newShootState;
         }
 
-        private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
+        public void ReloadInput(bool newReloadState)
+        {
+            reload = newReloadState;
+        }
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
-	
+        public void SlideInput(bool newSlideState)
+        {
+            slide = newSlideState;
+        }
+
+        public void PrimaryInput(bool newPrimaryState)
+        {
+            primary = newPrimaryState;
+        }
+
+        public void SecondaryInput(bool newSecondaryState)
+        {
+            secondary = newSecondaryState;
+        }
+
+        public void PauseInput(bool newPauseState)
+        {
+            pause = newPauseState;
+        }
+
+        public void CrouchInput(bool newCrouchState)
+        {
+            crouch = newCrouchState;
+        }
+
+        public void InteractInput(bool newInteractState)
+        {
+            interact = newInteractState;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
+
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+
 }
