c2f7659373b4e825d48603018cdaa255470ae4c5
Alfred <alfredkangjr@gmail.com>
2024-08-08 23:57:30 +0800

    fix: adding items and text size fix

--- Assets/Scripts/ItemsBuffs/Item.cs
+++ Assets/Scripts/ItemsBuffs/Item.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Item")]
 public class Item : ScriptableObject
 {
-    public int id;
     public string itemName;
     public string itemDescription;
     public Sprite icon;
