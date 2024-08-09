35a0988fb55f3c11da06d0248dfdb4d65da6e494
Alfred-Kang <alfredkangjr@gmail.com>
2024-08-09 15:47:41 +0800

    feat: test

--- Assets/Scripts/ItemsBuffs/ItemScripts/SharperArrows.cs
+++ Assets/Scripts/ItemsBuffs/ItemScripts/SharperArrows.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SharperArrows : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManager.instance.playerBaseDamage *= 1.2f;
     }
 }
